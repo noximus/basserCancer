ba0bc92c16ce56a997dcec2833dedc8727de6791
William <williamluo9001@gmail.com>
2016-09-23 15:51:19 -0400

    circle animation continues

--- 01_Infographic/js/Animation.js
+++ 01_Infographic/js/Animation.js
@@ -23,9 +23,39 @@ var BanTimeline = {
 };
 BanTimeline.init();
 
-$('.first-circle').circleProgress({
-  value: 0.75,
-  fill: {gradient: [['#b31423', .2], ['#d9182a', .5]], gradientAngle: Math.PI / 4}
-}).on('circle-animation-progress', function(event, progress, stepValue) {
-  $(this).find('strong').html(String(stepValue.toFixed(2)).substr(2)).append("%");
+var circle1 = false;
+var circle2 = false;
+var circle3 = false;
+
+$(window).scroll(function() {    
+  
+    var scroll = $(window).scrollTop();
+    if (scroll > 200 && circle1 == false) {
+
+      AnimateCircle('first-circle', .75);
+      circle1 = true;
+
+    }
+
+    if (scroll > 450 && circle2 == false) {
+      AnimateCircle('second-circle', .50);
+      circle2 = true;
+    }
+
+     if (scroll > 750 && circle3 == false) {
+      AnimateCircle('third-circle', .25);
+      circle3 = true;
+    }
+
+
 });
+
+
+function AnimateCircle(name, amount){
+  $('.'+ name).circleProgress({
+      value: amount,
+      fill: {gradient: [['#b31423', .2], ['#d9182a', .5]], gradientAngle: Math.PI / 4}
+    }).on('circle-animation-progress', function(event, progress, stepValue) {
+      $(this).find('strong').html(String(stepValue.toFixed(2)).substr(2)).append("%");
+    });
+}

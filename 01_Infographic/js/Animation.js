96865e04515b586a937155e84c6df8e6403b6d3e
William <williamluo9001@gmail.com>
2016-09-23 14:45:20 -0400

    got animation to work but so much more to do

--- 01_Infographic/js/Animation.js
+++ 01_Infographic/js/Animation.js
@@ -1,3 +1,4 @@
+
 var BanTimeline = {
   init: function () {
     // initial settings for banner frame 1.  In case banner needs to restart.  add as many as you can to CSS first so Banner doesn't jump
@@ -21,3 +22,10 @@ var BanTimeline = {
   }
 };
 BanTimeline.init();
+
+$('.third.circle').circleProgress({
+  value: 0.75,
+  fill: {gradient: [['#0681c4', .5], ['#4ac5f8', .5]], gradientAngle: Math.PI / 4}
+}).on('circle-animation-progress', function(event, progress, stepValue) {
+  $(this).find('strong').text(String(stepValue.toFixed(2)).substr(1));
+});

a3be0a608b2598c9d3f454d5ecd7373a881bf530
Henry Richardson <hrichardson83@gmail.com>
2016-09-26 12:58:29 -0400

    removed extra tweenmax js and changed filename

--- 01_Infographic/js/Animation.js
+++ 01_Infographic/js/Animation.js
@@ -1,28 +1,3 @@
-
-var BanTimeline = {
-  init: function () {
-    // initial settings for banner frame 1.  In case banner needs to restart.  add as many as you can to CSS first so Banner doesn't jump
-
-    // run animation after init is done. 
-    BanTimeline.frame1();
-  },
-  reset: function () {
-    
-  },
-  frame1: function () {
-    // TweenLite.to(f1_img1, 2, {opacity:1, display: 'block', delay: 0, ease: Quad.easeOut, yoyo: true});
-    // TweenLite.to(f1_img2, 2, {opacity:1, display: 'block', delay: 1, ease: Quad.easeOut, yoyo: true});
-  
-    TweenLite.delayedCall(3, BanTimeline.frame2);
-  },
-  frame2: function () {
-    // TweenLite.to(f2_img1, 2, {opacity:1, display: 'block', delay: 0, ease: Quad.easeOut});
-
-    // TweenLite.delayedCall(3, BanTimeline.frame3);
-  }
-};
-BanTimeline.init();
-
 var circle1 = false;
 var circle2 = false;
 var circle3 = false;
